27a475fb15737710e3d734542c943aa2ae9b65ff
Kylian V <virolle.kylian@gmail.com>
2022-03-11 10:37:01 +0100

    Basic HTML Page v7
    + Opérateur typeof

---
+++ assets/js/datatypes/dataTypeTypeof.js
@@ -0,0 +1,12 @@
+function dataTypeTypeof() {
+  // Exemple opérateur typeof
+  const maVariable = 42;
+
+  return typeof maVariable;
+}
+
+function resultDatatTypeof() {
+  alert("Résultat : " + dataTypeTypeof());
+}
+
+document.getElementById("datatTypeof").innerHTML = dataTypeTypeof;
